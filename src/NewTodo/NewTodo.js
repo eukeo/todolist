29d166f6c76930c251dd90dd34a6348f27b196e4
eukeo <sixsblooms@gmail.com>
2023-03-29 21:47:58 -0700

    new-todo-logic

--- src/NewTodo/NewTodo.js
+++ src/NewTodo/NewTodo.js
@@ -1,18 +1,68 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const NewTodo = (props) => {
   const [deleteTodo, setDeleteTodo] = useState(false);
+  const [editTodo, setEditTodo] = useState(false);
+  const [editTime, setEditTime] = useState(false);
+  const [text, setText] = useState(props.text);
+  const [time, setTime] = useState(props.time);
+  const [saveButton, setSaveButton] = useState(true);
+
+  useEffect(() => {
+    if (text.length > 2 && time.length !== "") {
+      setSaveButton(false);
+    } else {
+      setSaveButton(true);
+    }
+  }, [text, time]);
+
+  const editTodoHandler = () => {
+    setEditTodo(true);
+    setEditTime(true);
+  };
 
   const deleteTodoHandler = () => {
     setDeleteTodo(true);
+  };
+
+  const saveTodoHandler = () => {
+    setEditTodo(false);
+    setEditTime(true);
+    setText(text);
+    setTime(time);
+  };
+
+  const textChangeHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  const timeChangeHandler = (e) => {
+    setTime(e.target.value);
   };
 
   return (
     <div>
       {!deleteTodo && (
         <ul>
-          {props.text} {props.time}
-          <button onClick={deleteTodoHandler}>Remove</button>
+          {editTodo && editTime ? (
+            <ul>
+              <input type="text" value={text} onChange={textChangeHandler} />
+              <input type="time" value={time} onChange={timeChangeHandler} />
+            </ul>
+          ) : (
+            <li>
+              {text} {time}
+              <button onClick={editTodoHandler}>Edit</button>
+              <button onClick={deleteTodoHandler}>Remove</button>
+            </li>
+          )}
+          {editTodo && (
+            <div>
+              <button onClick={saveTodoHandler} disabled={saveButton}>
+                Save
+              </button>
+            </div>
+          )}
         </ul>
       )}
     </div>
